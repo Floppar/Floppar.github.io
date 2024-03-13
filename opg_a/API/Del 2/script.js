2fb3006fbe3e0bc5de97f30b831f0f42b59baabb
Floppar <145451047+Floppar@users.noreply.github.com>
2024-03-13 10:46:28 +0100

    API Update
    Introducing the API task to Konseptutvikling.

---
+++ opg_a/API/Del 2/script.js
@@ -0,0 +1,19 @@
+// Sync the getquote function
+
+async function getQuote() {
+    let url;
+    const data = await fetch("https://random.dog/woof.json");
+    // data waits for it to get a quote before continuing
+    // Get the values from the link
+    const json = await data.json();
+    // Wait to complete
+    url = json.url;
+    // The object is set to what value the link got
+
+    if (url) {
+        document.getElementById("url").src=url;
+    }
+}
+
+getQuote();
+// Get the value and display it
---
+++ opg_a/API/script.js
@@ -0,0 +1,26 @@
+// Sync the getquote function
+
+async function getQuote() {
+    let joke;
+    let author;
+    const data = await fetch("https://api.breakingbadquotes.xyz/v1/quotes");
+    // data waits for it to get a quote before continuing
+    // Get the values from the link
+    const json = await data.json();
+    // Wait to complete
+    joke = json[0].quote;
+    author = json[0].author;
+    // The object is set to what value the link got
+
+    if (joke) {
+        document.getElementById("joke").innerText = "''" + joke + "''";
+    }
+    // Get the current element (or value) of "joke" and display it between two '' 
+    if (author) {
+        document.getElementById("author").innerText = author;
+    }
+    // Same here but with the author name
+}
+
+getQuote();
+// Get the value and display it
